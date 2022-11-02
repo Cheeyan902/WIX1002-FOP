91579766ca9fafab0fc5e1c81028bbef511c451e
Cheeyan902 <tiewcheeyan@gmail.com>
2022-11-02 20:42:59 +0800

    ya

---
+++ L1Q2/src/l1q2/MyProfile.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package l1q2;
+
+/**
+ *
+ * @author tiewc
+ */
+public class MyProfile {
+    public static void main (String[] args){
+        
+        String a = "TIEW CHEE YAN";
+        int b = 22057371;
+        String c = "26, Jalan Wijaya 2, Taman Wijaya, 85300, Labis, Johor";
+        String d = "tiewcheeyan@gmail.com";
+        String e = "01163681627" ;
+        
+        System.out.println("My name is : " + a);
+        System.out.println("My matric number is : " + b);
+        System.out.println("My address is : " + c);
+        System.out.println("My gmail is: " + d);
+        System.out.println("My contact number is : " + e);
+        
+    }
+}
