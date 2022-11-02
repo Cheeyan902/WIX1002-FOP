caa8bc14f3ceffb00b173728d4af5fbdfb4a26f9
Cheeyan902 <tiewcheeyan@gmail.com>
2022-11-02 20:34:43 +0800

    ya

---
+++ L1Q4/src/l1q4/L1Q4.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q4;
+
+/**
+ *
+ * @author tiewc
+ */
+public class L1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("   Month");
+        System.out.println("     ^");
+        System.out.println(" JAN |#########################");
+        System.out.println(" FEB |################");
+        System.out.println(" MAR |####################");
+        System.out.println(" APR |###########################");
+        System.out.println(" MAY |################################");
+        System.out.println(" JUN |########");
+        System.out.println("     |----|----|----|----|----|----|----|----|--> Amount x 10^2");
+        System.out.println("     0    5    10   15   20   25   30   35   40");
+    }
+    
+}
